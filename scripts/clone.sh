d1b6b5a1a6a9682a8fcddba32a4367dd77bf1e24
Leonardo Sandoval <leonardo.sandoval@linaro.org>
2021-09-13 12:06:26 -0500

    Introduce a global share repository mechanism
    This is a full refactor of the way jobs fetch git data: with this
    approach, only the top level job, the upstream job, would set a share
    folder populated with all required repositories, including
    trusted-firmware-a, tf-a-tests, tf-a-job-configs and tf-a-ci-scripts,
    ultimately consumed by the builder job, tf-a-builder. This would
    reduce considerably the CI build times, avoiding multiple clones per
    job thus overloading the TF git servers as reported in [1].
    
    The way it works is simple: the top level job sets the SHARE_FOLDER
    parameter based on its name and number on top of the share
    volume (/srv/shared/<job name>/<job number>) then it calls the clone
    script (clone.sh), which in turn it fetches the repositories mentioned
    above. Jobs triggered on behalf of the latter, share the same
    SHARE_FOLDER value, and these in turn also call the clone script, but
    in this case, the script detects that the folder is already populated so
    its role is to simply copy the repositories into the job's
    workspace. As seen, all jobs work with repositories on their own
    workspace, which are just copies of the share folder, so there is no
    change of a race condition, i.e every job works with its own copy. The
    worst case scenario is where the down-level job, tf-a-builder, uses its
    default SHARE_FOLDER value, in this case, it would simply clone its
    own repositories without reusing any file however the current approach
    prevents the latter unless the job is triggered manually from the
    builder job itself.
    
    Besides the git-cloning strategy presented, the change implied moving
    all of jobs to 'docker-amd64-tf-a-bionic' container node, instead of
    'master', the former having access to the docker share volume.
    
    [1] https://linaro.atlassian.net/browse/TFC-20
    
    Signed-off-by: Leonardo Sandoval <leonardo.sandoval@linaro.org>
    Change-Id: I001bc1148cde4b1952161d84aa3d2e089a4281a2

---
+++ scripts/clone.sh
@@ -0,0 +1,96 @@
+#!/usr/bin/env bash
+#
+# Copyright (c) 2021 Arm Limited. All rights reserved.
+#
+# SPDX-License-Identifier: BSD-3-Clause
+#
+# Clones and checkout TF-A related repositories in case these are not present
+# under SHARE_FOLDER, otherwise copy the share repositories into current folder
+# (workspace)
+
+# The way it works is simple: the top level job sets the SHARE_FOLDER
+# parameter based on its name and number on top of the share
+# volume (/srv/shared/<job name>/<job number>) then it calls the clone
+# script (clone.sh), which in turn it fetches the repositories mentioned
+# above. Jobs triggered on behalf of the latter, share the same
+# SHARE_FOLDER value, and these in turn also call the clone script, but
+# in this case, the script detects that the folder is already populated so
+# its role is to simply copy the repositories into the job's
+# workspace. As seen, all jobs work with repositories on their own
+# workspace, which are just copies of the share folder, so there is no
+# change of a race condition, i.e every job works with its own copy. The
+# worst case scenario is where the down-level job, tf-a-builder, uses its
+# default SHARE_FOLDER value, in this case, it would simply clone its
+# own repositories without reusing any file however the current approach
+# prevents the latter unless the job is triggered manually from the
+# builder job itself.
+
+set -e
+
+# Global defaults
+REFSPEC_MASTER="refs/heads/master"
+GIT_REPO="https://git.trustedfirmware.org"
+GERRIT_HOST="https://review.trustedfirmware.org"
+GIT_CLONE_PARAMS="--depth=1 --no-checkout --no-tags"
+
+# Defaults Projects
+TF_GERRIT_PROJECT="${GERRIT_HOST}/${TF_GERRIT_PROJECT:-TF-A/trusted-firmware-a}"
+TFTF_GERRIT_PROJECT="${GERRIT_HOST}/${TFTF_GERRIT_PROJECT:-/TF-A/tf-a-tests}"
+SCRIPTS_PROJECT="${SCRIPTS_PROJECT:-${GIT_REPO}/ci/tf-a-ci-scripts.git}"
+JOBS_PROJECT="${JOB_PROJECT:-${GIT_REPO}/ci/tf-a-job-configs.git}"
+
+# Default Reference specs
+TF_GERRIT_REFSPEC="${TF_GERRIT_REFSPEC:-${REFSPEC_MASTER}}"
+TFTF_GERRIT_REFSPEC="${TFTF_GERRIT_REFSPEC:-${REFSPEC_MASTER}}"
+SCRIPTS_REFSPEC="${SCRIPTS_REFSPEC:-${REFSPEC_MASTER}}"
+JOBS_REFSPEC="${JOBS_REFSPEC:-${REFSPEC_MASTER}}"
+
+# Array containing "<repo url>;"<repo name>;<refspec>" elements
+repos=(
+    "${SCRIPTS_PROJECT};tf-a-ci-scripts;${SCRIPTS_REFSPEC}"
+    "${JOBS_PROJECT};tf-a-job-configs;${JOBS_REFSPEC}"
+    "${TF_GERRIT_PROJECT};trusted-firmware-a;${TF_GERRIT_REFSPEC}"
+    "${TFTF_GERRIT_PROJECT};tf-a-tests;${TFTF_GERRIT_REFSPEC}"
+)
+
+# Take into consideration non-CI runs where SHARE_FOLDER variable
+# may not be present
+if [ -z "${SHARE_FOLDER}" ]; then
+    # Default Jenkins values
+    SHARE_VOLUME="${SHARE_VOLUME:-$PWD}"
+    JOB_NAME="${JOB_NAME:-local}"
+    BUILD_NUMBER="${BUILD_NUMBER:-0}"
+    SHARE_FOLDER=${SHARE_VOLUME}/${JOB_NAME}/${BUILD_NUMBER}
+fi
+
+# clone git repos
+for repo in ${repos[@]}; do
+
+    # parse the repo elements
+    REPO_URL="$(echo "${repo}" | awk -F ';' '{print $1}')"
+    REPO_NAME="$(echo "${repo}" | awk -F ';' '{print $2}')"
+    REPO_REFSPEC="$(echo "${repo}" | awk -F ';' '{print $3}')"
+
+    # clone and checkout in case it does not exit
+    if [ ! -d ${SHARE_FOLDER}/${REPO_NAME} ]; then
+        git clone ${GIT_CLONE_PARAMS} ${REPO_URL} ${SHARE_FOLDER}/${REPO_NAME}
+
+        # fetch and checkout the corresponding refspec
+        cd ${SHARE_FOLDER}/${REPO_NAME}
+        git fetch ${REPO_URL} ${REPO_REFSPEC}
+        git checkout FETCH_HEAD
+        git log -1
+        cd $OLDPWD
+
+    else
+        # otherwise just show the head's log
+        cd ${SHARE_FOLDER}/${REPO_NAME}
+        git log -1
+        cd $OLDPWD
+    fi
+
+    # copy repository into pwd dir (workspace in CI), so each job would work
+    # on its own workspace
+    cp -a -f ${SHARE_FOLDER}/${REPO_NAME} ${PWD}/${REPO_NAME}
+
+done
