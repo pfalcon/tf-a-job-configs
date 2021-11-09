7c1186be1114eba6ac934fef88ad4d67974b80d2
Paul Sokolovsky <paul.sokolovsky@linaro.org>
2021-11-09 17:16:55 +0300

    tf-a-builder/postbuild.groovy: Decouple parts of description
    Previously, a description was posted only if both lava test was
    submitted and upstream cause was found. Decouple this logic,
    accumulate different parts of description (if any) separately,
    then set it at the end. This will allow to have these descriptions
    consistently, instead of wondering why some builds have them and
    why some don't.
    
    Signed-off-by: Paul Sokolovsky <paul.sokolovsky@linaro.org>
    Change-Id: I841540c9cfec5005567d2564403b1a4d54f61cba

--- tf-a-builder/postbuild.groovy
+++ tf-a-builder/postbuild.groovy
@@ -10,29 +10,18 @@ def getUpstreamRoot(cause) {
     return cause
 }
 
+def description = ""
+def rootUrl = manager.hudson.getRootUrl()
+
 // Add a LAVA job link to the description
 def matcher = manager.getLogMatcher("TEST JOB URL: (?<url>.*?) TEST JOB ID: (?<jobid>\\d+)")
 if (matcher?.matches()) {
     def testJobId = matcher.group('jobid')
     def testJobUrl = matcher.group('url')
-    def testDescription = "&nbsp;Test Job Id: <a href='${testJobUrl}'>${testJobId}</a>"
+    description += "&nbsp;Test Job Id: <a href='${testJobUrl}'>${testJobId}</a>"
 
-    def rootUrl = manager.hudson.getRootUrl()
     def lavaLogUrl = "${rootUrl}${manager.build.url}artifact/lava.log"
-    def lavaDescription = "<br >&nbsp;LAVA log: <a href='${lavaLogUrl}'>lava.log</a>"
-
-    def causes = manager.build.getAction(hudson.model.CauseAction.class).getCauses()
-    if (causes[0] instanceof hudson.model.Cause.UpstreamCause) {
-        def rootCause = getUpstreamRoot(causes[0])
-        def upstreamBuild = rootCause.upstreamBuild
-        def upstreamProject = rootCause.upstreamProject
-        def jobName = upstreamProject
-        def jobConfiguration = upstreamProject
-        def jobUrl = "${rootUrl}job/${upstreamProject}/${upstreamBuild}"
-        def jobDescription = "<br>&nbsp;Build <a href='${jobUrl}'>${upstreamProject} #${upstreamBuild}</a>"
-
-        manager.build.setDescription(testDescription + lavaDescription + jobDescription)
-    }
+    description += "<br >&nbsp;LAVA log: <a href='${lavaLogUrl}'>lava.log</a>"
 
     // Verify LAVA jobs results, all tests must pass, otherwise turn build into UNSTABLE
     def testMatcher = manager.getLogMatcher("LAVA JOB RESULT: (?<result>\\d+)")
@@ -44,3 +33,17 @@ if (matcher?.matches()) {
         }
     }
 }
+
+def causes = manager.build.getAction(hudson.model.CauseAction.class).getCauses()
+if (causes[0] instanceof hudson.model.Cause.UpstreamCause) {
+    def rootCause = getUpstreamRoot(causes[0])
+    def upstreamBuild = rootCause.upstreamBuild
+    def upstreamProject = rootCause.upstreamProject
+    def jobName = upstreamProject
+    def jobConfiguration = upstreamProject
+    def jobUrl = "${rootUrl}job/${upstreamProject}/${upstreamBuild}"
+    description += "<br>&nbsp;Build <a href='${jobUrl}'>${upstreamProject} #${upstreamBuild}</a>"
+}
+
+// Set accumulated description
+manager.build.setDescription(description)
